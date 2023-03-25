e7d574d74a7983a9fe9819ce49fec16d0b39f27a
Uhruum <jurica.drvo@gmail.com>
2023-03-25 15:20:05 +0100

    changed morgan to morgan body

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 import dotenv from 'dotenv';
 import express from 'express';
 import { InversifyExpressServer } from 'inversify-express-utils';
+import morganBody from 'morgan-body';
 import container from "./inversify.config";
 import './presentation/EmscController';
 import './presentation/ReverseGeocodingServiceController';
-import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+import * as fs from "fs";
+import * as path from "path";
 const swaggerDocument = require("./public/swagger.json");
 dotenv.config();
 
@@ -14,10 +16,20 @@ const port = process.env.PORT;
 
 const app = express();
 
+const log = fs.createWriteStream(
+    path.join(process.cwd(), "kraken.log"), { flags: "a"}
+);
+morganBody(app, {
+    // .. other settings
+    noColors: true,
+    maxBodyLength: -1,
+    stream: log,
+});
+
+
 let server =  new InversifyExpressServer(container, null, { rootPath: "/api" }, app);
 server.setConfig((app) => {
   app.use(express.json());
-  app.use(morgan(`${ process.env.MorganFormat }`));
   app.use(express.static("/src/public"));
   app.use(
     "/api/docs",
@@ -30,8 +42,8 @@ server.setErrorConfig( (app) => {
   app.use( ( err : Error , request : express.Request , response : express.Response , next : express.NextFunction ) => {
       console.error( err.stack );
       response.status( 500 ).send( "Something broke!" );
-  } );
-} );
+  });
+});
 
 let appConfigured = server.build();
 appConfigured.listen(port || 9000, () => console.log("Server is running on port", port));
